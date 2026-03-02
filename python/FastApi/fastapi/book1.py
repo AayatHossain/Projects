393b67e707fb16a53ce9684d977278d50f6945b8
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-03 01:25:28 +0600

    s

--- python/FastApi/fastapi/book1.py
+++ python/FastApi/fastapi/book1.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/book1")
-async def msg():
-    return {"message":"hello world"}
+books = [
+    {"Title":"A", "Author":"AA","Rating":"4.9"},
+    {"Title": "B", "Author": "BB", "Rating": "3.9"},
+    {"Title": "C", "Author": "CC", "Rating": "2.9"},
+
+]
+
+@app.get("/books")
+async def allbooks():
+    return books
