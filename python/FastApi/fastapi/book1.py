3c1811e0c501cee77ff5505052b746153b4ada53
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-11 05:11:31 +0600

    s

--- python/FastApi/fastapi/book1.py
+++ python/FastApi/fastapi/book1.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 books = [
-    {"Title":"A", "Author":"AA","Rating":"4.9"},
-    {"Title": "B", "Author": "BB", "Rating": "3.9"},
-    {"Title": "C", "Author": "CC", "Rating": "2.9"},
+    {"Title":"A", "Author":"AA","Rating":"3.0"},
+    {"Title": "B", "Author": "BB", "Rating": "4.0"},
+    {"Title": "C", "Author": "CC", "Rating": "5.0"},
 
 ]
 
 @app.get("/books")
 async def allbooks():
     return books
+
+@app.get("/books/{dynamic}")
+async def single_book(dynamic: str):
+    for book in books:
+        if book.get("Title").casefold()==dynamic.casefold():
+            return book
+    return {"No books found"}
