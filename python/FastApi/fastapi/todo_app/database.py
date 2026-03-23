96b746428f37cf5cef3ffe179ea6eed22ab425fb
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-23 13:52:25 +0600

    s

---
+++ python/FastApi/fastapi/todo_app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+
+URL = "sqlite:///./todos.db"
+engine = create_engine(URL, connect_args = {"check_same_thread" : False})
+Base = declarative_base()
+Sessionlocal = sessionmaker(autoflush=False, autocommit=False,bind=engine)
+
+
+
---
+++ python/FastApi/fastapi/todo_app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column,INTEGER,String, Boolean
+from database import Base
+
+class Todo(Base):
+    __todoname__ = "Todo Table"
+
+    id = Column(INTEGER, primary_key=True, index=True)
+    title = Column(String, index = True)
+    description = Column(String)
+    priority = Column(INTEGER)
+    completed = Column(Boolean, default = False)
