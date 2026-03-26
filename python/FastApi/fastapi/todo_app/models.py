345c186b0dd440f7045c7fb8bda91506f64dd7ef
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-27 03:53:04 +0600

    add user table and prepare for auth

--- python/FastApi/fastapi/todo_app/models.py
+++ python/FastApi/fastapi/todo_app/models.py
@@ -1,11 +1,22 @@
-from sqlalchemy import Column,INTEGER,String, Boolean
+from sqlalchemy import Column, INTEGER, String, Boolean, ForeignKey
 from database import Base
 
+class User(Base):
+    __tablename__ = "user"
+    id = Column(INTEGER, primary_key=True, index = True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    is_active = Column(String)
+    role = Column(String)
+
+
 class Todo(Base):
-    __tablename__ = "Todo_Table"
+    __tablename__ = "todo"
 
     id = Column(INTEGER, primary_key=True, index=True)
     title = Column(String, index = True)
     description = Column(String)
     priority = Column(INTEGER)
     completed = Column(Boolean, default = False)
+    owner = Column(ForeignKey("user.id"))
--- python/FastApi/fastapi/todo_app/routers/auth.py
+++ python/FastApi/fastapi/todo_app/routers/auth.py
@@ -1,7 +1,44 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database import Sessionlocal
+from models import User
 
 router = APIRouter()
 
+class UserRequest(BaseModel):
+
+    username : str
+    email : str
+    role : str
+    password : str
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield  db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth")
+async def create_user(db : db_dependency, user: UserRequest):
+    new_user = User(
+
+        username = user.username,
+        email=user.email,
+        hashed_password = user.password,
+        role = user.role,
+        is_active = True
+    )
+    if new_user.username is None:
+        raise HTTPException(status_code=401, detail = "Invalid username")
+    db.add(new_user)
+    db.commit()
+
 @router.get("/auth")
-async def auth_func():
-    return {"User Authenticated"}
+async def get_users(db : db_dependency):
+    return db.query(User).all()
