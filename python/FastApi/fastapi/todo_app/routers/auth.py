6477a8c2a1e542133784eb27527dc7b560a5925b
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-30 01:03:29 +0600

    s

--- python/FastApi/fastapi/todo_app/routers/auth.py
+++ python/FastApi/fastapi/todo_app/routers/auth.py
@@ -1,12 +1,16 @@
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
 from database import Sessionlocal
 from models import User
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 
 router = APIRouter()
+bcrypt_context =  CryptContext(schemes=['bcrypt'], deprecated = 'auto')
 
 class UserRequest(BaseModel):
 
@@ -30,7 +34,7 @@ async def create_user(db : db_dependency, user: UserRequest):
 
         username = user.username,
         email=user.email,
-        hashed_password = user.password,
+        hashed_password = bcrypt_context.hash(user.password),
         role = user.role,
         is_active = True
     )
@@ -42,3 +46,50 @@ async def create_user(db : db_dependency, user: UserRequest):
 @router.get("/auth")
 async def get_users(db : db_dependency):
     return db.query(User).all()
+
+
+def authentication(username: str, password: str, db: db_dependency):
+    user = db.query(User).filter(User.username==username).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="invalid credentials")
+    if not bcrypt_context.verify(password,user.hashed_password):
+        raise HTTPException(status_code=401, detail="invalid credentials")
+    return user
+
+
+SECRET_KEY = '9f3c1a7e8b2d4c6f9a0b1e2d3c4f5a6b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2'
+ALGORITHM = 'HS256'
+
+def create_token(user_id: int, username:str,expiry_time: timedelta):
+    encode_val = {'sub':username, 'id':user_id, 'exp' : datetime.now(timezone.utc)  + expiry_time}
+    token = jwt.encode(encode_val, SECRET_KEY, algorithm = ALGORITHM)
+    return token
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+@router.post("/token", response_model=Token)
+async def login_for_token(form: Annotated[OAuth2PasswordRequestForm, Depends()],
+                          db: db_dependency):
+    user = authentication(form.username,form.password,db)
+    if user is None:
+        raise HTTPException(status_code=401, detail="invalid credentials")
+    token = create_token(user.id, user.username, timedelta(minutes=20))
+    return {
+        "access_token" : token,
+        "token_type" : "bearer"
+    }
+
+oauth2_bearer = OAuth2PasswordBearer('token')
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username : str = payload.get('sub')
+        user_id : int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=401, detail="invalid credentials")
+        return {"username" : username, "user_id" : user_id}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="invalid credentials")
