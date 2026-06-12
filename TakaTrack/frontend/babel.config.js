e959d914c4b7de0af127075f550e7737841ce5b0
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-06-13 05:34:38 +0600

    Add reanimated worklets babel plugin

--- TakaTrack/frontend/babel.config.js
+++ TakaTrack/frontend/babel.config.js
@@ -2,5 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // Required by Reanimated 4 / react-native-worklets. Must be the LAST plugin.
+    plugins: ['react-native-worklets/plugin'],
   };
 };
